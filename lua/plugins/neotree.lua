d3840f622459694a793ee23f5a87f82623b5cd8a
Vahid Al <thevahidal@gmail.com>
2024-04-14 16:03:10 +0330

    Add initial nvim config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        -- hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+      window = {
+        position = "left",
+        width = 40,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+        mappings = {
+          ["l"] = "open",
+        },
+      },
+    },
+  },
+}
